810e77a11dc4c37dbfe81b84b9b2a280d1f9ec90
Jonathan Ferreira <jonathanferreira23@gmail.com>
2019-08-15 20:28:55 +0100

    Final Commit

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,15 @@
 import React, { useRef, useContext } from "react";
 import { useDrag, useDrop } from "react-dnd";
+
 import { Container, Label } from "./styles";
 
 import BoardContext from "../context";
 
-export default function Card({ data, index }) {
+export default function Card({ data, index, listIndex }) {
   const ref = useRef();
   const { move } = useContext(BoardContext);
   const [{ isDragging }, dragRef] = useDrag({
-    item: { type: "CARD", index, id: data.id },
+    item: { type: "CARD", index, listIndex },
     collect: monitor => ({
       isDragging: monitor.isDragging()
     })
@@ -17,10 +18,16 @@ export default function Card({ data, index }) {
   const [, dropRef] = useDrop({
     accept: "CARD",
     hover(item, monitor) {
+      const draggedListIndex = item.listIndex;
+      const targetListIndex = listIndex;
+
       const draggedIndex = item.index;
       const targetIndex = index;
 
-      if (draggedIndex === targetIndex) {
+      if (
+        draggedIndex === targetIndex &&
+        draggedListIndex === targetListIndex
+      ) {
         return;
       }
 
@@ -37,7 +44,10 @@ export default function Card({ data, index }) {
         return;
       }
 
-      move(draggedIndex, targetIndex);
+      move(draggedListIndex, targetListIndex, draggedIndex, targetIndex);
+
+      item.index = targetIndex;
+      item.listIndex = targetListIndex;
     }
   });
 
